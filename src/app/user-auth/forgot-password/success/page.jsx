098b62c365cff1e032dbc9dfc1bf4fa53ad68c23
Ferdinand <bokallitechnology@gmail.com>
2023-10-11 21:01:51 +0100

    Add margin

--- src/app/user-auth/forgot-password/success/page.jsx
+++ src/app/user-auth/forgot-password/success/page.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Success() {
     return (
         <div className="grid place-items-center h-screen">
-            <div className="shadow-lg p-5 rounded-lg border-t-4 border-blue-600">
+            <div className="shadow-lg p-5 mx-2 rounded-lg border-t-4 border-blue-600">
                 <h2 className="text-xl font-bold my-4">Password Reset</h2>
                 <h3 className="text-xl font-bold my-4">
                     If the email does not show up, check your spam folder.
