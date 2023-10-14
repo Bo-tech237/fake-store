52204a2b0dd70f0216d7833a39d351b3473b76fc
Ferdinand <bokallitechnology@gmail.com>
2023-10-14 09:52:18 +0100

    Remove session user email

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -8,12 +8,11 @@ export default function Dashboard() {
         <section>
             <div className="flex flex-wrap justify-between items-center">
                 <p className="font-bold">{session?.user?.name}</p>
-                <p className="font-bold">{session?.user?.email}</p>
                 <button
                     onClick={() => signOut()}
                     className="bg-red-500 text-white rounded font-bold px-6 py-2"
                 >
-                    Log Out
+                    Logout
                 </button>
             </div>
         </section>
