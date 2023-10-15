445d737a065cf71facd4e389a702b495a8e6d120
Ferdinand <bokallitechnology@gmail.com>
2023-10-15 09:15:09 +0100

    Modify settings

--- src/app/dashboard/settings/page.jsx
+++ src/app/dashboard/settings/page.jsx
@@ -1,3 +1,7 @@
-export default function Settings() {
+import { User } from '@/models/User';
+
+export default async function Settings() {
+    const user = await User.find({ role: 'Admins' });
+    console.log('settings', user);
     return <div>Settings</div>;
 }
