5d566d678095a09d726271ce91b8c6530335083f
Ferdinand <bokallitechnology@gmail.com>
2023-10-18 14:51:17 +0100

    Add loading component

---
+++ src/app/cart/loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <div className="bg-black text-white text-2xl h-screen grid place-items-center">
+            Loading...
+        </div>
+    );
+}
