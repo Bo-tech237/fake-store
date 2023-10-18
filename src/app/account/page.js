201edf92906fca41530d369cf667b27b00ce34ec
Ferdinand <bokallitechnology@gmail.com>
2023-10-18 14:56:11 +0100

    Create new component

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -1,22 +1,9 @@
-'use client';
-import { useSession, signOut } from 'next-auth/react';
+import AccountPage from '@/components/AccountPage';
 
 export default function Account() {
-    const { data: session } = useSession();
-
     return (
-        <section className="flex flex-col items-center justify-center min-h-screen gap-5">
-            <div>
-                {session?.user.role === 'User' && (
-                    <button
-                        onClick={() => signOut()}
-                        className="bg-red-500 text-white rounded font-bold px-6 py-2"
-                    >
-                        Log Out
-                    </button>
-                )}
-            </div>
-            <p>Welcome {session?.user.username}</p>
-        </section>
+        <div>
+            <AccountPage />
+        </div>
     );
 }
